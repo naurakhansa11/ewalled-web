51c7d8d6cbd8f554eee38f226a408c42fc04702a
Naura Khansa Alifa <knaura139@gmail.com>
2025-03-21 10:45:12 +0700

    chore: learning basic sql dan java

---
+++ basic-java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "BSI";
+        int x = 5;
+        int y = 6;
+        int sum = x+y;
+        System.out.println("Hello, " + name);
+        System.out.println("jumlah, " + sum);
+    }
+}
